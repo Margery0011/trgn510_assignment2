e4438031e83e483c44b815d5c9d70c18328d003d
Margery0011 <89502586+Margery0011@users.noreply.github.com>
2021-09-06 14:46:24 -0700

    Update Problem8.sh

--- Problem8.sh
+++ Problem8.sh
@@ -4,6 +4,6 @@ echo $FILES $1
 for f in $FILES
 do
    echo "Processing $f file..."
-   # count number of lines and output that for file $f
+   # count number of characters and output that for file $f
    wc -m $f
 done
